e662fdc21b97e30d8b0320275c02c8e9d1e23ab7
Chris Badahdah <a.s.badahdah@gmail.com>
2018-12-08 17:41:12 -0700

    Day 2

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code.core
   (:require [clojure.java.io :as io]
+            [clojure.string :as s]
             [clojure.test :refer [deftest is]]))
 
 ; --- Day 1: Chronal Calibration ---
@@ -40,6 +41,52 @@
            [[+7 +7 -2 -7 -4] 14]
            [(resource "2018-day-01-input.txt") 66932]]]
     (is (= (first-twice (resulting-frequency (cycle in))) out))))
+
+; --- Day 2: Inventory Management System ---
+
+; https://adventofcode.com/2018/day/2
+
+(defn has-value? [n]
+  (fn [ls] (some #(= % n) ls)))
+
+(defn checksum [inventory]
+  (let [counts (->> inventory (map frequencies) (map vals))]
+    (* (->> counts (filter (has-value? 2)) count)
+       (->> counts (filter (has-value? 3)) count))))
+
+; https://adventofcode.com/2018/day/2#part2
+
+(defn diff-string [a b]
+  (loop [i 0 found? false result ""]
+    (cond
+      ; end of string
+      (= i (count a))
+      (if found? result nil)
+      ; same character
+      (= (get a i) (get b i))
+      (recur (inc i) found? (str result (get a i)))
+      ; different character
+      found? nil
+      :else (recur (inc i) true result))))
+
+(defn common-letters [ls]
+  (some identity (for [a ls b ls] (diff-string a b))))
+
+(deftest inventory-management-system
+  (doseq [[in out]
+          [[["abcdef" "bababc"
+             "abbcde" "abcccd"
+             "aabcdd" "abcdee" "ababab"] 12]
+           [(s/split (slurp (io/resource "2018-day-02-input.txt")) #"\n")
+            7163]]]
+    (is (= (checksum in) out)))
+  (doseq [[in out]
+          [[["abcde" "fghij"
+             "klmno" "pqrst"
+             "fguij" "axcye" "wvxyz"] "fgij"]
+           [(s/split (slurp (io/resource "2018-day-02-input.txt")) #"\n")
+            "ighfbyijnoumxjlxevacpwqtr"]]]
+    (is (= (common-letters in) out))))
 
 (comment
   (clojure.test/run-tests *ns*))
