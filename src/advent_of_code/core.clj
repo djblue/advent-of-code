1085d3e6b1125296910913859489539ee2a75118
Chris Badahdah <a.s.badahdah@gmail.com>
2024-12-01 17:55:00 -0700

    2024 - Day 1 - Part 1 + Part 2

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -2,10 +2,12 @@
   (:require [clojure.test :refer [run-tests]]
             advent-of-code.core-2018
             advent-of-code.core-2019
-            advent-of-code.core-2020))
+            advent-of-code.core-2020
+            advent-of-code.core-2024))
 
 (defn -main []
   (run-tests 'advent-of-code.core-2018)
   (run-tests 'advent-of-code.core-2019)
-  (run-tests 'advent-of-code.core-2020))
+  (run-tests 'advent-of-code.core-2020)
+  (run-tests 'advent-of-code.core-2024))
 
