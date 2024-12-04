232fecf3f8c9a27f8eadbebfe870fe2f34550ac3
Chris Badahdah <a.s.badahdah@gmail.com>
2024-12-03 19:55:57 -0700

    2024 - Day 2 - Part 1 + Part 2

--- src/advent_of_code/core_2024.clj
+++ src/advent_of_code/core_2024.clj
@@ -39,3 +39,62 @@
   (is (= 2815556 (day-1-solution (slurp (io/resource "2024/01-input.txt")))))
   (is (= 31 (day-1-solution-2 input-01)))
   (is (= 23927637 (day-1-solution-2 (slurp (io/resource "2024/01-input.txt"))))))
+
+;; --- Day 2: Red-Nosed Reports ---
+
+(def input-02
+  "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9")
+
+(defn parse-input-day-2 [input]
+  (->> (str/split-lines input)
+       (map #(map parse-long (str/split % #"\s+")))))
+
+(defn sign [n] (if (> n 0) 1 -1))
+
+(defn all-increasing-or-decreasing [numbers]
+  (not-any?
+   (fn [[a b c]] (not= (sign (- a b)) (sign (- b c))))
+   (partition 3 1 numbers)))
+
+(defn at-least-one-at-most-three [numbers]
+  (not-any?
+   (fn [[a b]] (not (<= 1 (abs (- a b)) 3)))
+   (partition 2 1 numbers)))
+
+(defn safe-levels? [numbers]
+  (and (all-increasing-or-decreasing numbers)
+       (at-least-one-at-most-three numbers)))
+
+(defn day-2-solution-1 [input]
+  (let [input (parse-input-day-2 input)]
+    (->> input
+         (filter safe-levels?)
+         (count))))
+
+(defn all-but-one [numbers]
+  (for [n (range (count numbers))]
+    (keep-indexed
+     (fn [idx item]
+       (when (not= idx n)
+         item))
+     numbers)))
+
+(defn safe-levels-2? [numbers]
+  (some safe-levels? (all-but-one numbers)))
+
+(defn day-2-solution-2 [input]
+  (let [input (parse-input-day-2 input)]
+    (->> input
+         (filter safe-levels-2?)
+         (count))))
+
+(deftest day-2
+  (is (= 2 (day-2-solution-1 input-02)))
+  (is (= 631 (day-2-solution-1 (slurp (io/resource "2024/02-input.txt")))))
+  (is (= 4 (day-2-solution-2 input-02)))
+  (is (= 665 (day-2-solution-2 (slurp (io/resource "2024/02-input.txt"))))))
